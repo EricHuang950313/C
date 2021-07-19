27f71502ff65ac9cf546b37b8add08df5809e87a
EricHuang950313 <songzhi313@gmail.com>
2021-07-20 01:26:49 +0800

    1

--- Folders/learnC/b.c
+++ Folders/learnC/b.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
-void main(){
-    int n1, n2;
-    char c1[30];
-    scanf("%d %d\n", &n1, &n2);
-    gets(c1);
-    printf("n1=%d n2=%d\n", n1, n2);
-    printf("c1=%s", c1);
+
+#define DOG "Happy_dog"
+const int day = 1;
+
+int main(void) {   
+    printf("%s %d", DOG, day);
 }
