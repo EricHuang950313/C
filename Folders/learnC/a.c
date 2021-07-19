08c3b30a71b854ae6e36456f662052ad2a76f233
EricHuang950313 <songzhi313@gmail.com>
2021-07-19 20:42:55 +0800

    1

--- Folders/learnC/a.c
+++ Folders/learnC/a.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-void main() {
+void main() {   
    char a = 'A';
    char* b = "ABC";
    int c = 123;
@@ -11,7 +11,7 @@ void main() {
    unsigned long h = 123;
    float i = 1.23;
    double j = 1.23;
-   long double k = 5.5767458458;
+   // long double k = 6.123456789;
    printf("a: %c; %d\n", a, sizeof(a));
    printf("b: %s; %d\n", b, sizeof(b));
    printf("c: %d; %d\n", c, sizeof(c));
@@ -22,5 +22,5 @@ void main() {
    printf("h: %d; %d\n", h, sizeof(h));
    printf("i: %f; %d\n", i, sizeof(i));
    printf("j: %f; %d\n", j, sizeof(j));
-   printf("%Lf", k);
+   // printf("k: %.16Lf; %d\n", k, sizeof(k));
 }
